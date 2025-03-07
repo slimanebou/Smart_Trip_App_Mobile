9387776488a6fdadf0f016d00fa846c378b7b96f
Slimane <slimanbouaroua900@gamil.com>
2025-03-07 08:17:59 +0100

    Sing in & sing up page

---
+++ App/app/src/main/java/com/example/app/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+
+        val btn = findViewById<TextView>(R.id.textViewSingup)
+        btn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btn2 = findViewById<TextView>(R.id.btnSingip)
+        btn2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ App/app/src/main/java/com/example/app/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+        val btn = findViewById<TextView>(R.id.alreadyHaveAccount)
+        btn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val btn2 = findViewById<TextView>(R.id.btnSingup)
+        btn2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
