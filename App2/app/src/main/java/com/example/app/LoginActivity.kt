a0535d7589f1b43803dfaae20a6056de44213554
Slimane <slimanbouaroua900@gamil.com>
2025-04-04 22:25:40 +0200

    sign up page

--- App2/app/src/main/java/com/example/app/LoginActivity.kt
+++ App2/app/src/main/java/com/example/app/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.credentials.Credential
